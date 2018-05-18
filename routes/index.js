99a5f7bbea4a892962b27fe91d5855485ff73969
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-18 15:47:21 -0400

    fetch guardian headlines functional

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const config = require('../bin/config.js');
+const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  fetch('https://content.guardianapis.com/search?api-key=5d01b574-a525-43d4-8e09-7ee87215919a')
+    .then(res => res.json())
+    .then(json => res.render('index', { title: 'Guardian Headlines', news: json.response.results[0].webTitle }));
 });
 
 module.exports = router;
