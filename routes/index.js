4cc9becd33264c77ce2b17e038548d66d617f6e0
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-18 15:32:01 -0400

    require config.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('../bin/config.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
