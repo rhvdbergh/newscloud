7c6de8785570d9426262802eaf615f10093afebb
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-19 15:00:39 -0400

    tweets, builds str from tweets

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 const config = require('../bin/config.js');
+
 // to-source is is needed to pass the array as a normal string - but as source code, 
 // so toString() does not work (result of toString() on arrays = csv)
 const toSource = require('to-source'); 
 
+const Twit = require('twit');
+const T = new Twit(config.twitter);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   let news = [['foo', 12], ['bar', 6]];
   let tweets = [['tweet', 12], ['terrific', 6]];
   res.render('index', { title: 'NewsCloud', news: toSource(news), tweets: toSource(tweets) });
+});
+
+/* GET twitter page. */
+router.get('/twitter/', function(req, res, next) {
+
+  T.get('search/tweets', { q: `news`, count: 100 }, function(err, data, response) {
+    
+    let str = '';
+
+    data.statuses.forEach(tweet => {
+
+      str += tweet.text;
+      console.log(str);
+    });
+    res.json(data);
+  });
 });
 
 module.exports = router;
