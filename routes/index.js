aec032dcc96d9ae3d3c4a6d91b2e180ac0f7de21
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-05-19 14:30:41 -0400

    array passed to pug

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const config = require('../bin/config.js');
+// to-source is is needed to pass the array as a normal string - but as source code, 
+// so toString() does not work (result of toString() on arrays = csv)
+const toSource = require('to-source'); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NewsCloud' });
+  let news = [['foo', 12], ['bar', 6]];
+  console.log(toSource(news));
+  res.render('index', { title: 'NewsCloud', news: toSource(news) });
 });
 
 module.exports = router;
